4000cec1fc2648e3b4dfdfc1829dbe537b9fc47a
Joel Lanciaux <joel.lanciaux@gmail.com>
2015-11-21 22:01:07 -0500

    Now obtaining table state.

--- src/util/get-table-state.js
+++ src/util/get-table-state.js
@@ -1,5 +1,13 @@
 import * as actions from '../actions';
 
-export function getTableState(store) {
-  return {}; // TODO
+export default function getTableState(store) {
+  const state = store.getState();
+
+  return {
+    page: state.getIn(['pageProperties', 'currentPage']),
+    pageSize: state.getIn(['pageProperties', 'pageSize']),
+    filter: state.get('filter'),
+    sortColumn: state.getIn(['sortProperties', 'sortColumns']),
+    sortDirection: state.getIn(['sortProperties', 'sortAscending']) ? 'asc' : 'desc'
+  };
 }
